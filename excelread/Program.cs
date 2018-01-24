5f19e2aa72c6036c8f5bf33f002fc972eb81d750
Thomas <thomaspoly@gmail.com>
2018-01-25 09:58:08 +1100

    check in gsm

---
+++ excelread/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using excelread;
+
+namespace excelread
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            ExcelReaderFile ex = new ExcelReaderFile("D:\\excel\\test.xlsx");
+
+            string cell = ex.getCellData("test", 1, 2);
+
+            int rcount = ex.getRowCount("test");
+            int ccount = ex.getColumnCount("test");
+
+            Console.WriteLine(rcount);
+            Console.WriteLine(ccount);
+
+
+            Console.WriteLine(cell);
+        }
+    }
+}
